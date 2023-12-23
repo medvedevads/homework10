e9f6f949cc4ac1dc2482bb321ca9ddc7be57e586
Medvedeva Daria <medvedevads@biocad.ru>
2023-12-23 15:21:21 +0300

    First commit

---
+++ s10.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import random
+ 
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+data.head()
+pd.get_dummies(data['whoAmI'])
+
+# Ищем ункальные значения
+col = data['whoAmI'].unique()   # ['robot' 'human']
+# Создаем новый датафрейм
+datasec = pd.DataFrame(0, index = data.index, columns = col)    # 2 колонки, значения только "0"
+# Перебираем исходный датафрейм, если по индексу уникальное значение из исходного датафрейма записываем в новый
+for i in col:
+    datasec.loc[data['whoAmI'] == i, i] = 1     # меняем значения на "1"
+print(datasec)
+
